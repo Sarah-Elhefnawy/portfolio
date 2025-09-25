657c06a98cc2d7bea99895496e2d5dafcb5ba89e
Sarah <sarahelhefnawy0@gmail.com>
2025-09-25 23:44:51 +0300

    Navbar+Footer+Home(About Section)+Flowbite+FontAwsome+Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo:'home', pathMatch:'full' },
+    { path: 'home', loadComponent: () => import('./features/components/home/home.component').then(m => m.HomeComponent), title: 'Home'},
+    { path: 'about', loadComponent: () => import('./features/components/about/about.component').then(m => m.AboutComponent), title: 'About'},
+    { path: 'contact', loadComponent: () => import('./features/components/contact/contact.component').then(m => m.ContactComponent), title: 'Contact'},
+    { path: 'project', loadComponent: () => import('./features/components/project/project.component').then(m => m.ProjectComponent), title: 'Project'},
+    { path: 'service', loadComponent: () => import('./features/components/service/service.component').then(m => m.ServiceComponent), title: 'Service'},
+    { path: 'tech-stack', loadComponent: () => import('./features/components/tech-stack/tech-stack.component').then(m => m.TechStackComponent), title: 'Tech Stack'},
+    { path: '**', loadComponent: () => import('./features/components/not-found/not-found.component').then(m => m.NotFoundComponent), title: '404'}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { FlowbiteService } from './core/services/flowbite.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('portfolio');
+
+  constructor(private flowbiteService: FlowbiteService, private elementRef: ElementRef) {}
+  
+  private setBackgroundImage(): void {
+    const body = this.elementRef.nativeElement.ownerDocument.body;
+    body.style.backgroundImage = "url('/images/aesthetic-background-with-pink-neon-led-light-effect.jpg')";
+    body.style.backgroundRepeat = 'no-repeat';
+    body.style.backgroundPosition = 'center';
+    body.style.backgroundSize = 'cover';
+    body.style.minHeight = '100vh';
+    body.style.margin = '0';
+    body.style.backgroundAttachment = 'fixed';
+  }
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite) => { });
+
+    this.setBackgroundImage()
+  }
+
 }
+
